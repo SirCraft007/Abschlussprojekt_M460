824cde24a61b87deda8c5ed937f7f47c824a3200
SirCraft007 <seramol@gmx.ch>
2026-03-03 10:34:31 +0100

    Add subjects overview and details pages; enhance user session handling

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from flask import Flask, redirect, render_template, request, url_for, session
 import requests
@@ -15,42 +16,107 @@ app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
 app.secret_key = "jakljlkfjklfhfajljhoaifpoafjhioh" if dev else os.urandom(24)
 
 
+# Logged in
 @app.get("/")
 def index():
     token = session.get("access_token")
-    print(token)
     if token is not None:
         headers = {"x-access-token": f"{token}"}
-        print(headers)
         response = requests.get(api_url + "/user", headers=headers)
         if response.status_code == 200:
             response_json = response.json()
             username = response_json["user"]["username"]
             session["username"] = username
-            response = requests.get(api_url + "/user", headers=headers)
-            if response.status_code == 200:
-                response_json = response.json()
-                print(response_json)
-                total_average = response_json["user"]["total_average"]
-                total_exams = response_json["user"]["total_exams"]
-                total_points = response_json["user"]["total_points"]
-                print(total_average, total_exams, total_points)
-                return render_template(
-                    "loggedin.html",
-                    username=username,
-                    total_avrage=total_average,
-                    total_exams=total_exams,
-                    total_points=total_points,
-                )
-            return render_template("loggedin.html", username=username)
-
+            total_average = response_json["user"]["total_average"]
+            total_exams = response_json["user"]["total_exams"]
+            total_points = response_json["user"]["total_points"]
+            return render_template(
+                "loggedin.html",
+                username=username,
+                total_avrage=total_average,
+                total_exams=total_exams,
+                total_points=total_points,
+            )
         else:
-            return render_template("index.html", error="Error")
+            try:
+                json_response = response.json()
+                if json_response["message"] == "Token is invalid or expired":
+                    session.clear()
+                    return redirect(url_for("login"))
+            except json.decoder.JSONDecodeError:
+                session.clear()
+                return redirect(url_for("login"))
+            return render_template("index.html")
 
     else:
         return render_template("index.html")
 
 
+@app.route("/subjects")
+def subjects():
+    token = session.get("access_token")
+    if token is not None:
+        headers = {"x-access-token": f"{token}"}
+        response = requests.get(api_url + "/subjects", headers=headers)
+        if response.status_code == 200:
+            response_json = response.json()
+            subjects_list = response_json.get("subjects", [])
+
+            # Prepare data for rendering
+            subjects_data = [
+                {
+                    "name": subject["name"],
+                    "average": subject["average"],
+                    "num_exams": subject["num_exams"],
+                    "points": subject["points"],
+                    "id": subject["id"],
+                }
+                for subject in subjects_list
+            ]
+
+            return render_template("subjects.html", subjects=subjects_data)
+        else:
+            return render_template("index.html", error="Error")
+    return redirect(url_for("login"))
+
+
+@app.route("/subjects/<int:subject_id>")
+def subject(subject_id):
+    token = session.get("access_token")
+    if token is None:
+        return redirect(url_for("login"))
+    headers = {"x-access-token": f"{token}"}
+    subject_response = requests.get(
+        api_url + "/subjects/" + str(subject_id), headers=headers
+    )
+    grades_response = requests.get(
+        api_url + f"/subjects/{subject_id}/grades", headers=headers
+    )
+    grades_response_json = grades_response.json()
+    exams = [
+        {
+            "date": exam["date"],
+            "details": exam["details"] or "Keine Details",
+            "grade": exam["grade"],
+            "weight": exam["weight"],
+            "id": exam["id"],
+        }
+        for exam in grades_response_json["grades"]
+    ]
+    if subject_response.status_code != 200:
+        return redirect(url_for("index"))
+    subjects_response_json = subject_response.json()
+    subject = subjects_response_json["subject"]
+    subject_data = {
+        "name": subject["name"],
+        "average": subject["average"],
+        "num_exams": subject["num_exams"],
+        "plus_points": subject["points"],
+    }
+    return render_template("subjects_id.html", subject=subject_data, exams=exams)
+
+
+# Login flow
 @app.route("/login", methods=["GET", "POST"])
 def login():
     token = session.get("access_token")
